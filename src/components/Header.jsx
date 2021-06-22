99ba22c3742e51cca73309eb33471bc81cdea5d2
Vlad <54063227+legoyda@users.noreply.github.com>
2021-06-22 11:21:11 +0300

    First push

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+    return <header className="header">
+        <div align="left">
+            <div>
+                <img
+                    src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoWnzXNczzHaQvovq8M7rAPqqAaOt--KNlNg&usqp=CAU'
+                    height="50px" width="70px"></img>
+
+            </div>
+
+        </div>
+    </header>;
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className="content">
+        <div>
+            <img src='https://toyota.com.ua/uploads/camrynew2.png'
+                 width="900px" height="300px"></img>
+        </div>
+        <div>
+            ava+decr
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+
+    </div>
+}
+
+export default Profile;
